bc07f8570e27db3e8cc5066eb1054d0406bce7e4
apcdit <44289517+apcdit@users.noreply.github.com>
2018-10-31 21:32:11 +0800

    Update rules.vue

--- Vue/src/components/rules.vue
+++ Vue/src/components/rules.vue
@@ -1,5 +1,6 @@
 <template>
 <div>
+  <div class="container">
   <b-tabs>
     <b-tab title="参赛资格" active>
       <br>
@@ -9,20 +10,22 @@
         <li>参赛队伍人数为六人，包括一名领队及五名参赛者。领队可以参赛。</li>
         <li>六位辩论员当中，仅可以最多两位研究生（硕士生或博士生）参赛，其余的必须是大学本科生。</li>
         <li>有意参赛之各大学队伍需首先呈上《参赛意愿书》，以让大会妥善分配各地区参赛名额。各地区总参赛队伍数目为二十四支。<br>
-          （注：《参赛意愿书》不会公开 - 大会将以电子邮件方式发送给各个受邀请的大学队伍；若有意获取此《参赛意愿书》 请发送电邮至 apchinesedebate.adj@gmail.com 。）</li>
-        <li>有意参赛之队伍将使用本届赛会全新采用的“电子报名程序”（敬请参阅《亚太电子报名手册》）。</li>
+          <span style="color:red">*</span>《参赛意愿书》不会公开 - 大会将以电子邮件方式发送给各个受邀请的大学队伍<br>
+          <span style="color:red">*</span>若有意获取此《参赛意愿书》 请发送电邮至 apchinesedebate.adj@gmail.com</li>
+        <li>有意参赛之队伍将使用本届赛会全新采用的“电子报名程序”
+          <span style="color:red" >*</span>请参阅《亚太电子报名手册》）。</li>
       </ul>
     </b-tab>
     <b-tab title="奖励">
       <br>
-      <ol>
+      <ul>
           <li>冠军 ：奖状（个人）+ 奖杯（团体）+ 奖金新币 2000 + 常年杯<br>（三届连得冠军之队伍将永久保留常年杯）</li>
           <li>亚军 ：奖状（个人）+ 奖杯（团体）+ 奖金新币 1000</li>
           <li>季军 ：奖状（个人）+ 奖杯（团体）+ 奖金新币 600 （两队）</li>
           <li>全场最佳辩论员 ：奖状 + 奖杯 + 奖金新币 200</li>
           <li>单场最佳辩论员 ：奖状</li>
           <li>参赛证书</li>
-      </ol>
+      </ul>
     </b-tab>
     <b-tab title="电子报名手册">
         <center>
@@ -36,10 +39,25 @@
     </b-tab>
   </b-tabs>
 </div>
+</div>
 </template>
 <script>
     export default{
       name:'rules',
     }
 </script>
-<style></style>
+<style scoped>
+.container{background-color: #F7F7F7;
+  padding: 20px 25px 30px;
+  margin: 0 auto 25px;
+  margin-top: 50px;
+  /* shadows and rounded borders */
+  -moz-border-radius: 20px;
+  -webkit-border-radius: 20px;
+  border-radius: 20px;
+  -moz-box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+  -webkit-box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+  box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+  width:100%;}
+  ul{list-style:square;}
+</style>
