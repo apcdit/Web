984df4054b9bde9af1d2464be6300502ee8fc596
jiale <tanjiale1029@gmail.com>
2019-05-30 14:13:36 +0800

    change title font size

--- Vue/src/components/home.vue
+++ Vue/src/components/home.vue
@@ -6,7 +6,7 @@
             <div class="center text-center">
                 <!-- <div><img src="http://i.imgur.com/90YSaaO.png" /></div> -->
                 <div style="text-align: center;" class="col-xs-12">
-                    <div id="title"><strong>工行杯亚太大专辩论公开赛</strong></div>
+                    <div id="title"><strong>工行杯<br>亚太大专辩论公开赛</strong></div>
                     <!-- <div id="title"><strong></strong></div> -->
                     <br>
                     <h5><i class="fa fa-calendar-o" aria-hidden="true"></i> 2019年7月10日至7月14日</h5>
@@ -221,7 +221,7 @@
     }
 
     div #title{
-        font-size: 1.95rem;
+        font-size: 5vh;
         padding-top: 5%;
         font-family: "Microsoft YaHei","华文细黑";
         font-weight: bold;
